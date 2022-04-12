d139c901af85e8cc75d890361f8af23708593cff
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-12 14:32:50 +0200

    Fix linters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
