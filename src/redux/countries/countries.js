afa37e4423469c6d284b97baaf1e6b9987cb26f8
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-13 22:55:44 +0200

    Format countries data

--- src/redux/countries/countries.js
+++ src/redux/countries/countries.js
@@ -17,7 +17,50 @@ const getCountries = () => async (dispatch) => {
   await fetch('https://restcountries.com/v3.1/all')
     .then((data) => data.json())
     .then((data) => {
-      const countries = data;
+      const countries = [];
+      data.forEach((country) => {
+        const {
+          name,
+          cca2,
+          cca3,
+          independent,
+          unMember,
+          currencies,
+          capital,
+          region,
+          subregion,
+          languages,
+          latlng: coords,
+          population,
+          timezones,
+          continents,
+          flags: { svg: flag },
+          coatOfArms: { svg: emblem },
+          area,
+        } = country;
+
+        const formattedCountry = {
+          name,
+          cca2,
+          cca3,
+          independent,
+          unMember,
+          currencies,
+          capital,
+          region,
+          subregion,
+          languages,
+          coords,
+          population,
+          timezones,
+          continents,
+          flag,
+          emblem,
+          area,
+        };
+
+        countries.push(formattedCountry);
+      });
       dispatch(fetchCountries(countries));
     });
 };
