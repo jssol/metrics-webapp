b05dc9cbc12750bdea94979436b8f29c64caae84
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-13 17:44:39 +0200

    Add routes, header and home components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App() {
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Header from './components/Header';
+import Home from './components/Home';
+
+const App = () => {
+  const theme = useSelector((state) => state.theme);
+
   return (
-    <div>App</div>
+    <div className="w-screen h-screen overflow-x-hidden flex flex-col">
+      <Header />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={(<Home theme={theme} />)}
+        />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
