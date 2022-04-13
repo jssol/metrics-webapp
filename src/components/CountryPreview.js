5b6f0f64b39b2e8475036e4716fcec69622efec7
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-13 23:09:37 +0200

    Add map background

--- src/components/CountryPreview.js
+++ src/components/CountryPreview.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import randomNumber from '../logic/randomNumber';
+import '../sass/CountryPreview.scss';
 
 const CountryPreview = (props) => {
   const { hsl, country } = props;
-  const { name: { common }, capital, cca2: iso } = country;
+  const { name: { common }, capital, cca2 } = country;
 
   const shade = `${hsl + randomNumber(30, 60)}%)`;
-  const bgImage = `url("../data/countries/${iso.toLowerCase()}/vector.svg`;
+  const bgImage = `url("../data/countries/${cca2.toLowerCase()}/vector.svg`;
 
   const style = {
     backgroundColor: shade,
@@ -16,7 +17,7 @@ const CountryPreview = (props) => {
   };
 
   return (
-    <NavLink to={`/details/${common.toLowerCase()}`} className="h-full w-full text-center text-3xl text-white" style={style}>
+    <NavLink to={`/details/${common.toLowerCase()}`} className="h-full w-full text-center text-xl text-white p-2 CountryPreview" style={style}>
       <h2>
         {common}
       </h2>
