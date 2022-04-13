e2e543edb4bbd30cb679b50d1e0a72569a200e76
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-13 23:07:10 +0200

    Randomize theme color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CountryPreview from './CountryPreview';
 
 const Home = (props) => {
-  const { theme } = props;
-
-  const style = {
-    backgroundColor: theme,
-  };
+  const { theme, countries } = props;
+  const { hsl } = theme;
 
   return (
-    <main className="h-full w-full text-center text-3xl text-white" style={style}>
-      Home
+    <main className="h-full w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+      {
+        countries.map((country) => (
+          <CountryPreview
+            key={country.cca2.toLowerCase()}
+            hsl={hsl}
+            country={country}
+          />
+        ))
+      }
     </main>
   );
 };
 
 Home.propTypes = {
-  theme: PropTypes.string.isRequired,
+  theme: PropTypes.instanceOf(Object).isRequired,
+  countries: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default Home;
