141e16afb68bf403cd356ec0406639216f0e005b
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-15 00:37:26 +0200

    Add conditional rendering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FaCog, FaSearch } from 'react-icons/fa';
+import { FaCog, FaSearch, FaChevronLeft } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { closeDetails } from '../redux/details/details';
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const detailsOpen = useSelector((state) => state.details);
   const { theme } = props;
   const { hex } = theme;
-
   const style = {
     backgroundColor: hex,
   };
 
+  const handleClick = () => {
+    dispatch(closeDetails());
+  };
+
   return (
     <header className="w-full flex fixed top-0 z-10 shadow-md items-center justify-between px-6 py-4" style={style}>
-      <h1 className="text-3xl text-white font-bold">
-        <span className="text-black">Cultur</span>
-        escape
-      </h1>
-      <section className="flex items-center text-2xl">
-        <FaSearch />
-        <FaCog className="ml-3" />
-      </section>
+      {!detailsOpen && (
+        <>
+          <h1 className="text-3xl text-white font-bold">
+            <span className="text-black">Cultur</span>
+            escape
+          </h1>
+          <section className="flex items-center text-2xl">
+            <FaSearch />
+            <FaCog className="ml-3" />
+          </section>
+        </>
+      )}
+      {detailsOpen && <NavLink onClick={handleClick}><FaChevronLeft /></NavLink>}
     </header>
   );
 };
