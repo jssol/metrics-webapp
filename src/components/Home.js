8f3b6d89fa232de3b023bd1c88a1d877ce3f5b57
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-16 17:20:46 +0200

    Add overflow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector, shallowEqual } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import CountryPreview from './CountryPreview';
 import ErrorBoundary from './ErrorBoundary';
+import { endSearch } from '../redux/search/search';
 
 const Home = ({ theme, countries }) => {
+  const dispatch = useDispatch();
   const search = useSelector((state) => state.search, shallowEqual);
 
   const { hsl } = theme;
   const { searchOpen, searchResult } = search;
+
+  const handleClose = () => {
+    dispatch(endSearch());
+  };
 
   return (
     <main className="h-full w-full relative top-16 z-0 grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
@@ -25,20 +32,25 @@ const Home = ({ theme, countries }) => {
       <ErrorBoundary>
         {
           searchOpen && (searchResult.length !== 0) && (
-            <ul className="absolute rounded shadow-2xl w-10/12 md:w-5/12 z-20 top-3 right-3 bg-white text-blue-500 flex flex-col">
+            <section className="absolute rounded overflow-hidden shadow-2xl w-10/12 md:w-5/12 z-20 top-3 right-3 bg-white text-blue-500 flex flex-col">
               {
                 searchResult.map((country) => (
-                  <li className="flex justify-between even:bg-slate-100 p-2 items-center" key={country.cca2}>
+                  <NavLink
+                    to={`/${country.name.common.toLowerCase()}/details`}
+                    className="flex justify-between even:bg-slate-100 p-2 items-center"
+                    key={country.cca2}
+                    onClick={handleClose}
+                  >
                     <span>
                       {country.name.common}
                       {', '}
                       {country.capital[0]}
                     </span>
                     <FaRegArrowAltCircleRight className="text-base ml-1" />
-                  </li>
+                  </NavLink>
                 ))
               }
-            </ul>
+            </section>
           )
         }
       </ErrorBoundary>
