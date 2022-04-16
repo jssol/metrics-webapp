a1b9ce9054f06543de0e28a2c31d24d72f0aaa74
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-16 02:19:01 +0200

    Fetch country data

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import randomNumber from '../logic/randomNumber';
 import General from './General';
+import { getCountryData } from '../redux/country/country';
 
-const CountryDetails = (props) => {
+const CountryDetails = ({ theme, countries }) => {
   const params = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const { country } = params;
+    countries.forEach((count) => {
+      if (count.name.common.toLoweCase() === country.toLoweCase()) {
+        dispatch(getCountryData(count.cca2.toLoweCase()));
+      }
+    });
+  }, []);
 
-  const { theme: { hsl } } = props;
-  const { country } = params;
+  const { hsl } = theme;
+  const { country: countryName } = params;
 
   const shade = `${hsl + randomNumber(45, 53)}%)`;
   const style = {
@@ -18,9 +29,9 @@ const CountryDetails = (props) => {
   return (
     <main className="h-full w-full relative flex flex-col items-center top-16 z-0 text-white" style={style}>
       <section className="w-full h-10">
-        Country details
-        {' '}
-        {country}
+        {countryName.charAt(0).upperCase() + countryName.slice(1)}
+        {' / '}
+        Details
       </section>
       <General />
     </main>
@@ -29,6 +40,7 @@ const CountryDetails = (props) => {
 
 CountryDetails.propTypes = {
   theme: PropTypes.instanceOf(Object).isRequired,
+  countries: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default CountryDetails;
