04ca4fff2ee7d6041fb362186d9054439e6f1723
Jonathan Sivahera <jsivahera@icloud.com>
2022-04-16 16:18:14 +0200

    Add search function

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,16 +14,15 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(searchCountry(country));
-    setCountry('');
   };
 
   return (
-      <form className="w-1/3 flex items-center relative" onSubmit={handleSubmit}>
-        <input className="w-full rounded-lg text-slate-600 transition-all outline-blue-100 focus:outline-blue-300" type="search" name="country" onChange={handleChange} placeholder="Search country by name" title="Enter a country name or iso code" value={country} />
-        <button className="flex items-center justify-center rounded-full" type="submit">
-          <FaSearch />
-        </button>
-      </form>
+    <form className="w-full flex items-center text-sm md:text-md lg:text-xl text-blue-500 relative ml-3" onSubmit={handleSubmit}>
+      <input className="w-full relative rounded-full transition-all outline-blue-100 focus:outline-blue-300 flex h-8 lg:pt-1 px-3" type="text" name="country" onChange={handleChange} placeholder="Search country" title="Enter country name" value={country} />
+      <button className="flex items-center text-sm justify-center absolute right-3 rounded-full" type="submit">
+        <FaSearch />
+      </button>
+    </form>
   );
 };
 
